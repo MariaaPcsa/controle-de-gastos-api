728af53f37d9b44a31a45acd2208c426c5034492
MariaaPcsa <marialujivan@gmail.com>
2026-02-12 01:03:24 -0300

    carrecao swagger

--- user/src/main/java/com/maria/finance/user/infrastructure/security/OpenApiConfig.java
+++ user/src/main/java/com/maria/finance/user/infrastructure/security/OpenApiConfig.java
@@ -2,16 +2,21 @@ package com.maria.finance.user.infrastructure.security;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Controle de Gastos API - Módulo Usuários")
+                        .description("API responsável pelo gerenciamento de usuários, autenticação e permissões.")
+                        .version("v1.0.0")
+                )
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
